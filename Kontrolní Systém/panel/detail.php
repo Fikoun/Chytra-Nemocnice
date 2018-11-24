53c39d114e27f5a28fdd044fe1d5b7cfc888c6bf
AdiarFi <filipraida9@gmail.com>
2018-11-24 03:15:26 +0100

    nadpis vrací na index

--- Kontrolní Systém/panel/detail.php
+++ Kontrolní Systém/panel/detail.php
@@ -43,7 +43,7 @@ var_dump($patient);
         <!-- ============================================================== -->
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="index.html">Smart Hospital</a>
+                <a class="navbar-brand" href="index.php">Smart Hospital</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
